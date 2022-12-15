d822e7f60f771fea3a26e50ed1ac3cf321cf5d42
patrick kisyula <pmusyimi42@gmail.com>
2022-12-15 15:00:59 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+let body = document.querySelector('body')
+const submitData = (userName, userEmail)=>{
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'content-type':'application/json',
+            'accept':'application/json'
+        },
+        body:JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+    .then(res=> {
+        return res.json()
+    })
+    .then(data => {
+        body.innerHTML =` <p>${data.id}</p>`
+    })
+    .catch(err => {
+        body.innerHTML = `${err.message}`
+    });
+}
